9f3384a628f0a1c91a8e51d81d2d98ae4c55d892
colton <adamscolton64@gmail.com>
2023-01-11 11:15:51 -0600

    DoubleOps final

--- DoubleOps.java
+++ DoubleOps.java
@@ -7,7 +7,9 @@ class DoubleOps {
             System.out.println("args 1 squared - " + Math.pow(i1, 2));
             System.out.println("args 1 sqare root = " + Math.sqrt(i1));
             System.out.println("math.random * args 1 = " + Math.random() * i1);
+
             System.out.println();
+            
             System.out.println("args 2 abs = " + Math.abs(i2));
             System.out.println("args 2 squared - " + Math.pow(i2, 2));
             System.out.println("args 2 sqare root = " + Math.sqrt(i2));
